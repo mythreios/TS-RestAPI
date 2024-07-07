c3274a69cc46ef5efdfd73f2e5764c096ea2a8be
mythreios <realioncreative@gmail.com>
2024-07-07 18:21:38 +0300

    Update mongooseConnection.ts

--- src/Others/mongooseConnection.ts
+++ src/Others/mongooseConnection.ts
@@ -4,7 +4,7 @@ class Handler {
 
     async Init() {
         mongoose.set('strictQuery', false);
-        mongoose.connect("mongodb+srv://royalise:1381@cluster0.nefprti.mongodb.net/")
+        mongoose.connect("deletedPassword<Security>")
             .then(x => console.log(`DB OK!`))
                 .catch((err) => console.log(`DB ERROR! \n[${err}]`))
     }
